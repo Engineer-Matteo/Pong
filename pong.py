50ae10b77d9e2d96de93005820509d325c519bf7
Matteo Marissens <matteo.marissens@gmail.com>
2025-06-12 11:17:11 +0200

    Replay option added

--- pong.py
+++ pong.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-window_width, window_height = 1540, 800 #1920, 1080 == seperate monitor #1540, 800 == laptop screen
+window_width, window_height = 1540, 800
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Pong")
 
@@ -21,6 +21,8 @@ y_paddle2 = window_height / 2 - 50
 font = pygame.font.Font(None, 74)
 score1 = 0
 score2 = 0
+
+start_state = [locationX, locationY, ball_speed_x, ball_speed_y, ball_acceleration, y_paddle1, y_paddle2, score1, score2]
 
 def Basic_Ball_Movement(locationX, locationY, ball_speed_x, ball_speed_y, ball_acceleration, score1, score2):
     pygame.draw.ellipse(window, (255, 255, 255), (locationX, locationY, 25, 25))
@@ -101,14 +103,27 @@ while running == True:
 
     # If the difference in scores is 5, game ends
     if abs(score1 - score2) >= 5:
+        game_end = True
         if score1 > score2:
             game_over = font.render("You Win!", True, (255, 255, 255))
         else:
             game_over = font.render("GAME OVER", True, (255, 255, 255))
+        text = pygame.font.Font(None, 25).render("Press Enter to play again, Escape to quit", True, (255, 255, 255))
         window.fill((0, 0, 0))
         window.blit(game_over, (window_width/2 - game_over.get_width()/2, window_height/2 - game_over.get_height()/2))
+        window.blit(text, (window_width/2 - text.get_width()/2, window_height - text.get_height() - 5))
         pygame.display.flip()
-        pygame.time.wait(3000)  # Wait for 3 seconds before quitting
-        running = False
-    
+        while game_end == True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_end = False
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        locationX, locationY, ball_speed_x, ball_speed_y, ball_acceleration, y_paddle1, y_paddle2, score1, score2 = start_state
+                        game_end = False
+                    if event == pygame.K_ESCAPE:
+                        game_end = False
+                        running = False
+
     pygame.display.flip()
